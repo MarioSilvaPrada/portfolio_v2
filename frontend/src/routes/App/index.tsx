d7f049ac6f2940268e960ecd436235134252baa7
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-05-26 17:31:12 +0100

    removing submodule

---
+++ frontend/src/routes/App/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import * as S from './style';
+
+const App: FC = () => (
+  <S.Container>
+    <h1>Home</h1>
+  </S.Container>
+);
+
+export default App;
---
+++ frontend/src/routes/PageNotFound/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import * as S from './style';
+
+const PageNotFound: FC = () => (
+  <S.Wrapper>
+    <h1>Page Not Found</h1>
+  </S.Wrapper>
+);
+
+export default PageNotFound;
