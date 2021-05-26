d911435955e98ef32fa8e3faedcd4f954028fe06
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-05-26 18:09:11 +0100

    update

--- frontend/src/routes/App/index.tsx
+++ frontend/src/routes/App/index.tsx
@@ -1,9 +1,21 @@
 import React, { FC } from 'react';
 import * as S from './style';
 
+import Profile from 'assets/pic.png';
+
 const App: FC = () => (
   <S.Container>
-    <h1>Home</h1>
+    <S.ImageWrapper>
+      <S.ProfilePic src={Profile} />
+    </S.ImageWrapper>
+
+    <S.GreetingsWrapper>
+      <S.Text>Hello, I am</S.Text>
+      <S.MainText>
+        Mário <br /> Prada
+      </S.MainText>
+      <S.JobTitle>Software Developer</S.JobTitle>
+    </S.GreetingsWrapper>
   </S.Container>
 );
 
--- frontend/src/routes/App/style.tsx
+++ frontend/src/routes/App/style.tsx
@@ -1,5 +1,46 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`height: 100%;`;
+export const Container = styled.div`
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
 
-export const Title = styled.div`color: red;`;
+export const Title = styled.div`
+  color: red;
+`;
+
+export const ImageWrapper = styled.div`
+  position: absolute;
+  left: -10rem;
+  bottom: 0;
+`;
+
+export const ProfilePic = styled.img`
+  width: 55rem;
+`;
+
+export const GreetingsWrapper = styled.div`
+  position: absolute;
+  bottom: 40%;
+`;
+
+export const Text = styled.h1`
+  color: white;
+  text-shadow: 2px 20px 10px rgba(0, 0, 0, 0.25);
+  font-size: 4rem;
+`;
+
+export const MainText = styled.h1`
+  color: white;
+  text-shadow: 2px 20px 10px rgba(0, 0, 0, 0.25);
+  font-size: 8.4rem;
+  line-height: 8rem;
+`;
+
+export const JobTitle = styled.p`
+  color: ${({ theme }) => theme.colors.main};
+  font-size: 3rem;
+  text-shadow: 2px 20px 10px rgba(0, 0, 0, 0.25);
+`;
