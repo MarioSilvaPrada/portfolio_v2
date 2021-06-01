3bb9f62a765f66cec0b0d6486c7e230abe3317d9
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-06-01 22:35:38 +0100

    update

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# docker-compose exec app python manage.py makemigrations core
+
 from pathlib import Path
 import os
 
