5d39c4d5cc421d8c72565f822446ea9874df8d48
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-06-11 14:26:08 +0100

    update env variables

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5cm3t*3_-o#hjniuoy*n4)vo92&&io5s3-m)h4@r91&a&$se96'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
