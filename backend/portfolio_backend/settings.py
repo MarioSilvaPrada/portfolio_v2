f009a8928e3fa8a98ba9e05ab34f815a0f9c03fb
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-06-11 14:03:06 +0100

    added secure settings

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -31,6 +31,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio-mariosilvaprada.herokuapp.com']
 
+if DEBUG == 0:
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # new
+
 
 # Application definition
 
