c45aea48c35b2c8b2c87f5989cc7984961965969
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-06-11 15:19:06 +0100

    update

--- frontend/src/routes/App/index.tsx
+++ frontend/src/routes/App/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useEffect } from 'react';
-import { Services, Portfolio, Main, Technologies, Footer } from 'feed';
-import * as S from './style';
-
+import {
+  Services, Portfolio, Main, Technologies, Footer,
+} from 'feed';
 import { getProjects } from 'api/projects';
+import * as S from './style';
 
 const App: FC = () => {
   useEffect(() => {
