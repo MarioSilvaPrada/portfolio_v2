c65e8f85309b27bb21306f2fe6df2fa83b65062d
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-06-28 11:31:54 +0100

    update content

--- frontend/src/content/index.ts
+++ frontend/src/content/index.ts
@@ -1,8 +1,8 @@
 export const content = {
-  services: `Let's face it, development tends to be complex sometimes, and is for that reason I like to research and plan in order to deliver the best results to my clients.
-  My goal is always to write clean and readable code where not just me but also the client can easily maintain it to proper fit into a specific profile. I'm also passionate
-  to implement beautiful User Interface(UI) & User Experience(UX). My projects will work and look good on any device, no matter the size of the screen.`,
-  technologies: `As a developer I try to be agnostic regarding chosen technologies. However, there are some of them that brings me more joy to work with and as a 
-  result, I can present well-structured and completed products. React along with Redux and Typescript allow me to create beautiful User Interface in a clean and 
-  elegant way. In the other side, Django and Django Rest Framework offers an excellent way to build a solid server side logic with everything you need to feed your frontend.`,
+  services: `Let's face it, development tends to be complex sometimes, and is for that reason I like to research and plan to deliver the best results to my clients. 
+  My goal is always to write clean and readable code, where not just me but also the client can easily maintain it to properly fit into a specific profile. I'm also 
+  passionate to implement beautiful User Interface(UI) & User Experience(UX). My projects will work and look good on any device, no matter the size of the screen.`,
+  technologies: `As a developer, I try to be agnostic regarding chosen technologies. However, some of them bring me more joy to work with and as a result, I can present 
+  well-structured and completed products. React along with Redux and Typescript allow me to create a beautiful User Interface cleanly and elegantly. On the other side, 
+  Django and Django Rest Framework offers an excellent way to build a solid server-side logic with everything you need to feed your frontend.`,
 };
