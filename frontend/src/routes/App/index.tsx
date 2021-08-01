5748db43db1ac599be50002092cfb6b6fc49b25c
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-08-01 15:22:27 +0100

    update loading state

--- frontend/src/routes/App/index.tsx
+++ frontend/src/routes/App/index.tsx
@@ -9,7 +9,7 @@ import { ITalks, IProjects } from 'utils/interface';
 import * as S from './style';
 
 const App: FC = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [projects, setProjects] = useState<Array<IProjects>>([]);
   const [talks, setTalks] = useState<Array<ITalks>>([]);
 
