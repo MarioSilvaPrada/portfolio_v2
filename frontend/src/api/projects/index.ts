f88bac3ea6eceb24cfa9b635b81a36dd86f727aa
Mário Prada <marioprada@Marios-MBP.lan>
2022-08-01 18:47:51 +0100

    fix: testing

--- frontend/src/api/projects/index.ts
+++ frontend/src/api/projects/index.ts
@@ -2,7 +2,7 @@ import api from 'api';
 
 const getProjects = async () => {
   try {
-    const res = await api.get('api/portfolio/projects');
+    const res = await api.get('portfolio/projects');
     return res;
   } catch (err) {
     return err.response;
