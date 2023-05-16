b3fa38097af98f49ec4222b2d4da269c09b74014
MarioSilvaPrada <mariosilvaprada@gmail.com>
2023-05-16 15:52:38 +0100

    fix

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -6,7 +6,7 @@ export const USE_HOME_SERVER = true;
 const getUrl = () => {
   if (PRODUCTION) {
     console.log({ is: process.env.REACT_APP_API_ROOT });
-    if (USE_HOME_SERVER) return 'https://homeserver.mariosilvaprada.com/';
+    if (USE_HOME_SERVER) return 'https://homeserver.marioprada.me/';
     return process.env.API_URL;
   }
 
